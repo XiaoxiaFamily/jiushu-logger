eff2e5711d972ed35817415f008e02656cf55483
SakigamiYang <sakigami@163.com>
2023-08-13 17:57:41 +0800

    update dist

--- jiushu_logger/__init__.py
+++ jiushu_logger/__init__.py
@@ -1,6 +1,8 @@
 # coding: utf-8
 from .log import *
 
+__version__ = '0.1.1'
+
 __all__ = ['Logger', 'BizLogExtra', 'ReqLogExtra', 'CallLogExtra',
            'CronLogExtra', 'MiddlewareLogExtra', 'MqLogExtra',
            'CallType', 'MiddlewareType', 'MqType', 'MqHandleType']
