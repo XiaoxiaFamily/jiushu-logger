f608f40de78a6f0613ed601e9abb0927428b4b63
SakigamiYang <sakigami@163.com>
2023-08-17 21:58:42 +0800

    delete unused constant

--- jiushu_logger/_helpers.py
+++ jiushu_logger/_helpers.py
@@ -3,26 +3,7 @@ import typing
 
 import orjson
 
-__all__ = ['ENV_HEADERS', 'safely_jsonify']
-
-ENV_HEADERS = (
-    'X-Varnish',
-    'X-Request-Start',
-    'X-Heroku-Queue-Depth',
-    'X-Real-Ip',
-    'X-Forwarded-Proto',
-    'X-Forwarded-Protocol',
-    'X-Forwarded-Ssl',
-    'X-Heroku-Queue-Wait-Time',
-    'X-Forwarded-For',
-    'X-Heroku-Dynos-In-Use',
-    'X-Forwarded-Protocol',
-    'X-Forwarded-Port',
-    'X-Request-Id',
-    'Via',
-    'Total-Route-Time',
-    'Connect-Time'
-)
+__all__ = ['safely_jsonify']
 
 
 def safely_jsonify(obj: typing.Any) -> str:
