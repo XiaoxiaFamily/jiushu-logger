2d12693e5b89608a60b827a30956cca5fff3532c
Sakigami Yang <sakigami@163.com>
2023-12-21 01:01:29 +0800

    fix ai_model_log.py, take consideration for empty url

--- jiushu_logger/__init__.py
+++ jiushu_logger/__init__.py
@@ -3,7 +3,7 @@ from .ai_model_log import *
 from .helpers import *
 from .log import *
 
-__version__ = '1.0.5'
+__version__ = '1.0.6'
 
 __all__ = ['safely_jsonify',
            'Logger', 'BizLogExtra', 'ReqLogExtra', 'CallLogExtra',
